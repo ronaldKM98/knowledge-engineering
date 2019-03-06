70fa9ddbb079d6d70915666de0d2fefe969d4dab
JuanGQCadavid <jquiro12@eafit.edu.co>
2019-03-06 08:39:53 -0500

    Tree

--- binary_classification/Tree_plot.py
+++ binary_classification/Tree_plot.py
@@ -13,10 +13,12 @@ x = df[params_list].copy()
 clf = tree.DecisionTreeClassifier()
 clf = clf.fit(x.values,y.values.ravel())
 
-dot_data = tree.export_graphviz(clf, out_file='None', 
+
+dot_data = tree.export_graphviz(clf, out_file=None, 
                 feature_names=list(x),  
                 class_names='quality',  
                 filled=True, rounded=True,  
                 special_characters=True)  
-graph = graphviz.Source(dot_data)  
-graph.render('None')
+
+graph = graphviz.Source(dot_data, format='svg')  
+graph.render()
