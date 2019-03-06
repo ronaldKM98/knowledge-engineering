fc1371bb035c1b595feefff1c313187720fba437
Ronald Cardona Martinez <ronaldkm98@gmail.com>
2019-03-06 10:06:01 -0500

    Entregable Final y README

--- binary_classification/Tree_plot.py
+++ binary_classification/Tree_plot.py
@@ -20,5 +20,5 @@ dot_data = tree.export_graphviz(clf, out_file=None,
                 filled=True, rounded=True,  
                 special_characters=True)  
 
-graph = graphviz.Source(dot_data, format='svg')  
+graph = graphviz.Source(dot_data, format='png')
 graph.render()
