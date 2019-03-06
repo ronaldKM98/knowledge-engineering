c376e7eed688249252c8dcbce047ae9d5f4f50d6
JuanGQCadavid <jquiro12@eafit.edu.co>
2019-03-06 08:28:14 -0500

    Plot tree

---
+++ binary_classification/Tree_plot.py
@@ -0,0 +1,22 @@
+import graphviz
+from sklearn import tree
+import pandas as pandas
+
+params_list = ['fixed acidity', 'volatile acidity', 'citric acid', 'residual sugar', 'free sulfur dioxide', 'sulphates', 'alcohol']
+df = pandas.read_csv("whitewine.csv",sep=';')
+
+y = df[['quality']].copy()
+x = df.drop(columns=['quality'])
+
+x = df[params_list].copy()
+
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(x.values,y.values.ravel())
+
+dot_data = tree.export_graphviz(clf, out_file='None', 
+                feature_names=list(x),  
+                class_names='quality',  
+                filled=True, rounded=True,  
+                special_characters=True)  
+graph = graphviz.Source(dot_data)  
+graph.render('None')
